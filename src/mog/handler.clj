b6bc9f072d80eac9ab54a032dad38ffb6f461a6b
Bernard Schmitz <bernardschmitz@gmail.com>
2014-01-04 01:53:11 -0800

    blah

--- src/mog/handler.clj
+++ src/mog/handler.clj
@@ -3,15 +3,19 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(ref high-scores [ {:name "bilbo" :score 98765 } { :name "xtro" :score 3737 } ])
+
 (def score 12345)
-(def high-scores [ {:name "bilbo" :score 98765 } { :name "xtro" :score 3737 } ])
-(def top-score ((first high-scores) :score))
+(def top-score ((first @high-scores) :score))
 
 (defn main-page [req]
-  (html [:p "Score: " score]))
+  (html [:p "High Score: " top-score] [:br] [:br] [:br] [:p "Score: " score] [:br] [:br]
+        [:p "name: " ((req :session) :name)]))
 
 (defroutes app-routes
   (GET "/" [] main-page)
+  (GET "/login/:name" [name] 
+      {:redirect "/" :session {:name name :score 0}}) 
   (route/resources "/")
   (route/not-found "Not Found"))
 
