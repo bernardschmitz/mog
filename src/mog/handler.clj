81a684737b380a78d85054b889f41c2a476aced8
Bernard Schmitz <bernardschmitz@gmail.com>
2014-01-06 13:25:42 -0800

    trie?

--- src/mog/handler.clj
+++ src/mog/handler.clj
@@ -37,6 +37,27 @@
 (defn random-letters [] 
   (take 20 (shuffle letter-pool)))
 
+
+
+; 
+
+
+
+(defn add-to-trie [trie x]
+    (assoc-in trie x (merge (get-in trie x) {:val x :terminal true})))
+
+(defn in-trie? [trie x]
+    "Returns true if the value x exists in the specified trie."
+    (:terminal (get-in trie x) false))
+
+;(defn prefix-matches [trie prefix]
+;    "Returns a list of matches with the prefix specified in the trie specified."
+;    (map-filter :val (tree-seq map? vals (get-in trie prefix))))
+
+(defn build-trie [coll]
+    "Builds a trie over the values in the specified seq coll."
+    (reduce add-to-trie {} coll))
+
 (defn init-game-state [name]
   {:name name :hp 1000 :score 0 :fighting true :letters (random-letters)}) 
 
